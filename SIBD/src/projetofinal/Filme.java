3dc48fe8cf266b6d760ea979410eab0b4870efca
douglascmoura <105322602+douglascmoura@users.noreply.github.com>
2022-06-05 15:52:21 -0300

    Projeto Final de SIBD
    adicionei em uma nova branch as classes que eu fiz do projeto final.

---
+++ SIBD/src/projetofinal/Filme.java
@@ -0,0 +1,50 @@
+package projetofinal;
+public class Filme extends Sessão {
+	//Atributos
+	private String nomeFilme;
+	private String idFilme;
+	private	int valorFilme;
+	//Métodos 
+	private String getNomeFilme() {
+		return nomeFilme;
+	}
+	private void setNomeFilme(String nomeFilme) {
+		this.nomeFilme = nomeFilme;
+	}
+	private String getIdFilme() {
+		return idFilme;
+	}
+	private void setIdFilme(String idFilme) {
+		this.idFilme = idFilme;
+	}
+	private int getValorFilme() {
+		return valorFilme;
+	}
+	private void setValorFilme(int valorFilme) {
+		this.valorFilme = valorFilme;
+	}
+	//Métodos 
+	public void adicionarFilme(String nomeFilme, String idFilme, int valorFilme) {
+		setNomeFilme(nomeFilme);
+		setIdFilme(idFilme);
+		setValorFilme(valorFilme);
+	}
+	public String verFilme() {
+		String f = "Nome: " + this.getNomeFilme() +
+					"\nValor: " + this.getValorFilme() + " reais";
+		return f;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
