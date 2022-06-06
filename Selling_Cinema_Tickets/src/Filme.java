49e04d0c8597b6562b0349f020a091394647bb60
gustaavobraga <gustaa1320@gmail.com>
2022-06-06 19:38:26 -0300

    Trocando de pasta os arquivos do Douglas

--- Selling_Cinema_Tickets/src/Filme.java
+++ Selling_Cinema_Tickets/src/Filme.java
@@ -1,4 +1,46 @@
 
-public class Filme {
-
+public class Filme extends Sessao {
+	//Atributos
+	private String nomeFilme;
+	private String idFilme;
+	private	int valorFilme;
+	
+	//Métodos 
+	private String getNomeFilme() {
+		return nomeFilme;
+	}
+	
+	private void setNomeFilme(String nomeFilme) {
+		this.nomeFilme = nomeFilme;
+	}
+	
+	private String getIdFilme() {
+		return idFilme;
+	}
+	
+	private void setIdFilme(String idFilme) {
+		this.idFilme = idFilme;
+	}
+	
+	private int getValorFilme() {
+		return valorFilme;
+	}
+	
+	private void setValorFilme(int valorFilme) {
+		this.valorFilme = valorFilme;
+	}
+	
+	//Métodos 
+	public void adicionarFilme(String nomeFilme, String idFilme, int valorFilme) {
+		setNomeFilme(nomeFilme);
+		setIdFilme(idFilme);
+		setValorFilme(valorFilme);
+	}
+	
+	public String verFilme() {
+		String f = "Nome: " + this.getNomeFilme() +
+					"\nValor: " + this.getValorFilme() + " reais";
+		return f;
+	}
+	
 }
--- Selling_Cinema_Tickets/src/Sessao.java
+++ Selling_Cinema_Tickets/src/Sessao.java
@@ -1,4 +1,48 @@
 
-public class Sessao {
+public class Sessao  {
+	//Atributos
+	private String idSessão;
+	private String diaSessão;
+	private String horaSessão;
+	
+	//Métodos
+	private String getIdSessão() {
+		return idSessão;
+	}
+
+	private void setIdSessão(String idSessão) {
+		this.idSessão = idSessão;
+	}
+
+	private String getDiaSessão() {
+		return diaSessão;
+	}
+
+	private void setDiaSessão(String diaSessão) {
+		this.diaSessão = diaSessão;
+	}
+
+	private String getHoraSessão() {
+		return horaSessão;
+	}
+
+	private void setHoraSessão(String horaSessão) {
+		this.horaSessão = horaSessão;
+	}
+	
+	//Métodos 
+	public void adicionarSessão(String idSessão, String diaSessão, String horaSessão) {
+		setIdSessão(idSessão);
+		setDiaSessão(diaSessão);
+		setHoraSessão(horaSessão);
+	}
+	
+	public String verSessão() {
+		String s = "Sala: " + this.getIdSessão() +
+					"\nDia: " + this.getDiaSessão() +
+					"\nHora: " + this.getHoraSessão();
+		return s;
+		
+	}
 
 }
